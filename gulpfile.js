3af27de7fb691b91195aec08daa929731a16e904
Rafał Jońca <rafal@gluwer.com>
2017-02-19 17:59:40 +0100

    Added more foods Added weekly list with summary and local storage memory. Updated to newer polymer-build Small other fixes.

--- gulpfile.js
+++ gulpfile.js
@@ -23,6 +23,7 @@ const cssSlam = require('css-slam').gulp;
 const htmlMinifier = require('gulp-html-minifier');
 const mergeStream = require('merge-stream');
 const polymerBuild = require('polymer-build');
+const HtmlSplitter = polymerBuild.HtmlSplitter;
 
 const swPrecacheConfig = require('./sw-precache-config.js');
 const polymerJson = require('./polymer.json');
@@ -48,48 +49,36 @@ function waitFor(stream) {
 
 function build() {
   return new Promise((resolve, reject) => { // eslint-disable-line no-unused-vars
-    // Okay, so first thing we do is clear the build directory
     console.log(`Deleting ${buildDirectory} directory...`);
     del([buildDirectory])
       .then(() => {
-        // Okay, now lets get your source files
+        const sourcesHtmlSplitter = new HtmlSplitter();
+
         let sourcesStream = polymerProject.sources()
-          // Oh, well do you want to minify stuff? Go for it!
-          // Here's how splitHtml & gulpif work
-          .pipe(polymerProject.splitHtml())
-          .pipe(gulpif(/\.js$/, uglify()))
-          .pipe(gulpif(/\.css$/, cssSlam()))
-          .pipe(gulpif(/\.html$/, htmlMinifier(htmlMinifierConf)))
+          .pipe(sourcesHtmlSplitter.split())
           .pipe(gulpif(/\.(png|gif|jpg|svg)$/, imagemin()))
-          .pipe(polymerProject.rejoinHtml());
+          .pipe(sourcesHtmlSplitter.rejoin());
 
-        // Okay, now lets do the same to your dependencies
         let dependenciesStream = polymerProject.dependencies()
-          .pipe(polymerProject.splitHtml())
-          .pipe(gulpif(/\.js$/, uglify()))
-          .pipe(gulpif(/\.css$/, cssSlam()))
-          .pipe(gulpif(/\.html$/, htmlMinifier(htmlMinifierConf)))
-          .pipe(polymerProject.rejoinHtml());
 
-        // Okay, now lets merge them into a single build stream
         let buildStream = mergeStream(sourcesStream, dependenciesStream)
           .once('data', () => {
             console.log('Analyzing build dependencies...');
           });
+        
+        const buildHtmlSplitter = new HtmlSplitter();
+        buildStream = buildStream.pipe(polymerProject.bundler)
+          .pipe(buildHtmlSplitter.split())
+          .pipe(gulpif(/\.js$/, uglify()))
+          .pipe(gulpif(/\.css$/, cssSlam()))
+          .pipe(gulpif(/\.html$/, htmlMinifier(htmlMinifierConf)))
+          .pipe(buildHtmlSplitter.rejoin());
 
-        // If you want bundling, pass the stream to polymerProject.bundler.
-        // This will bundle dependencies into your fragments so you can lazy
-        // load them.
-        buildStream = buildStream.pipe(polymerProject.bundler);
-
-        // Okay, time to pipe to the build directory
         buildStream = buildStream.pipe(gulp.dest(buildDirectory));
 
-        // waitFor the buildStream to complete
         return waitFor(buildStream);
       })
       .then(() => {
-        // Okay, now lets generate the Service Worker
         console.log('Generating the Service Worker...');
         return polymerBuild.addServiceWorker({
           project: polymerProject,
