86d15d69ac2779e968b5de7586aa2263ccaa7cf9
Rafał Jońca <rafal@gluwer.com>
2017-03-04 20:11:26 +0100

    MVP ready except foods list.

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/polydiet/bower_components/webcomponentsjs/webcomponents-lite.min.js","02395895d5d08242c6ba93518a6da2c5"],["/polydiet/data/foods.json","8f584867d84e911a7bb0d72267d2ff31"],["/polydiet/index.html","2668e552d32139123de9b3ad5fed75bc"],["/polydiet/manifest.json","f4dbf1b6f917f7be782733f5d0ad2a0f"],["/polydiet/src/glu-polydiet.html","dfcc730a3f9f9cbdcbc2b6b3ddb78aaa"]];
+var precacheConfig = [["/polydiet/bower_components/webcomponentsjs/webcomponents-lite.min.js","02395895d5d08242c6ba93518a6da2c5"],["/polydiet/data/foods.json","8f584867d84e911a7bb0d72267d2ff31"],["/polydiet/index.html","2668e552d32139123de9b3ad5fed75bc"],["/polydiet/manifest.json","f4dbf1b6f917f7be782733f5d0ad2a0f"],["/polydiet/src/glu-polydiet.html","15e37a8bf349a29a55cd243387a0307a"]];
 var cacheName = 'sw-precache-v2--' + (self.registration ? self.registration.scope : '');
 
 
