6c596522a30bc195ec0ddd69c4100550563c5fc2
Rafał Jońca <rafal@gluwer.com>
2017-03-05 17:14:24 +0100

    Added details and several improvements.

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/polydiet/bower_components/webcomponentsjs/webcomponents-lite.min.js","02395895d5d08242c6ba93518a6da2c5"],["/polydiet/data/foods.json","8f584867d84e911a7bb0d72267d2ff31"],["/polydiet/index.html","2668e552d32139123de9b3ad5fed75bc"],["/polydiet/manifest.json","f4dbf1b6f917f7be782733f5d0ad2a0f"],["/polydiet/src/glu-polydiet.html","15e37a8bf349a29a55cd243387a0307a"]];
+var precacheConfig = [["/polydiet/bower_components/webcomponentsjs/webcomponents-lite.min.js","02395895d5d08242c6ba93518a6da2c5"],["/polydiet/data/foods.json","c234631a9dc3597adf69fc91cad1fd41"],["/polydiet/index.html","2668e552d32139123de9b3ad5fed75bc"],["/polydiet/manifest.json","f4dbf1b6f917f7be782733f5d0ad2a0f"],["/polydiet/src/glu-polydiet.html","c747ffd3fc745c474c408ea39548650a"]];
 var cacheName = 'sw-precache-v2--' + (self.registration ? self.registration.scope : '');
 
 
